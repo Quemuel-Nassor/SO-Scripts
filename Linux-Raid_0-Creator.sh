fd432939143b30517f1c2cf91e51732a3bd53a23
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2019-12-16 11:11:12 -0300

    Fix mksfs.fat sintax and implemented partitions mounting

--- Linux-Raid_0-Creator.sh
+++ Linux-Raid_0-Creator.sh
@@ -75,19 +75,23 @@ raid_partition(){
 
     echo -e "\n\nPreparing BOOT partition on /dev/md/RAID-0p1"
     (echo n; echo p; echo 1; echo ; echo +512M; echo t; echo 1; echo w) | fdisk /dev/md/RAID-0
-    mkfs.fat -F32 -v -L ROOT -m 0.5 -b 4096 -E stride=$((chunk/512)),stripe-width=$(((chunk/512)*2)) /dev/md/RAID-0p1
+    mkfs.fat -F32 -v -n ROOT /dev/md/RAID-0p1
+    mkdir -p /mnt/boot/efi && mount /dev/md/RAID-0p1 /mnt/boot/efi
 
     echo -e "\n\nPreparing ROOT partition on /dev/md/RAID-0p2"
     (echo n; echo 2; echo ; echo +30G; echo t; echo 2; echo 24; echo w) | fdisk /dev/md/RAID-0
     mkfs.ext4 -v -L ROOT -m 0.5 -b 4096 -E stride=$((chunk/4096)),stripe-width=$(((chunk/4096)*2)) /dev/md/RAID-0p2
+    mkdir /mnt && mount /dev/md/RAID-0p2 /mnt
 
     echo -e "\n\nPreparing HOME partition on /dev/md/RAID-0p3"
     (echo n; echo 3; echo ; echo +30G; echo t; echo 3; echo 28; echo w) | fdisk /dev/md/RAID-0
     mkfs.ext4 -v -L HOME -m 0.5 -b 4096 -E stride=$((chunk/4096)),stripe-width=$(((chunk/4096)*2)) /dev/md/RAID-0p3
+    mkdir /mnt/home && mount /dev/md/RAID-0p3 /mnt/home
 
     echo -e "\n\nPreparing SWAP partition on /dev/md/RAID-0p4"
     (echo n; echo 4; echo ; echo +8G; echo t; echo 4; echo 19; echo w) | fdisk /dev/md/RAID-0
     mkswap -L SWAP /dev/md/RAID-0p4
+    swapon /dev/md/RAID-0p4
 
     echo -e "\n\nSuccessfully created partitions on /dev/md/RAID-0"
     fdisk /dev/md/RAID-0 -l
