693d1341ea6cf7bdd6c44ac4701e7912f0e784a4
Quemuel Alves Nassor <41460212+Quemuel-Nassor@users.noreply.github.com>
2020-01-26 13:59:48 -0300

    Update Arch-Linux-Installer.sh

--- Arch-Linux-Installer.sh
+++ Arch-Linux-Installer.sh
@@ -161,7 +161,7 @@ install_base(){
 }
 install_complement(){
     echo -e "\n\nInstaling essentials packages"
-    (echo ; echo 1; echo Y) | pacman -S grub-efi-x86_64 efibootmgr os-prober ntfs-3g intel-ucode alsa-utils pulseaudio pulseaudio-alsa xorg-server xorg-xinit xorg-xrandr arandr mesa xf86-video-intel net-tools networkmanager wireless_tools dhcpcd nano vim
+    (echo ; echo 1; echo Y) | pacman -S grub-efi-x86_64 efibootmgr os-prober ntfs-3g intel-ucode alsa-utils pulseaudio pulseaudio-alsa xorg-server xorg-xinit xorg-xrandr arandr mesa xf86-video-intel net-tools networkmanager wireless_tools netctl dhcpcd nano vim
     # Remove the comment to install these packages
     #(echo ; echo 1; echo Y) | pacman -S screenfetch vlc p7zip firefox noto-fonts git xbindkeys htop sddm lm-sensors acpi i3status i3lock i3-wm xfce4-terminal xorg-twm xterm xclock xorg-xinit 
     finish_install
