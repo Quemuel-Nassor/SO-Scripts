6a6250091f507d1e41bcdbf54b48e6261109356b
Quemuel Alves Nassor <41460212+Quemuel-Nassor@users.noreply.github.com>
2025-06-07 17:03:31 -0300

    fix key path

--- Linux/post-install-setup.sh
+++ Linux/post-install-setup.sh
@@ -48,12 +48,12 @@ rm packages-microsoft-prod.deb
 
 # Add VsCode repository
 cat <<EOF | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/code stable main
+deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/microsoft-prod.gpg] https://packages.microsoft.com/repos/code stable main
 EOF
 
 # Add Edge repository
 cat <<EOF | sudo tee /etc/apt/sources.list.d/microsoft-edge.list > /dev/null
-deb [arch=amd64 signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main
+deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/microsoft-prod.gpg] https://packages.microsoft.com/repos/edge stable main
 EOF
 
 ########### Configure APT preferences ###########
